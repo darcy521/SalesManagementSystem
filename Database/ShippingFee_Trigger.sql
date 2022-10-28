bcd89d8d5b5dc1838a946abd1207879f81bc3204
Honglin Chen <94886510+darcy521@users.noreply.github.com>
2022-10-28 15:45:30 -0700

    add updating shippingFee trigger

--- Database/ShippingFee_Trigger.sql
+++ Database/ShippingFee_Trigger.sql
@@ -1,3 +1,25 @@
-CREATE Trigger ShippingFee_Trigger
-After insert on ShippingFee
-for each row
+CREATE OR REPLACE Trigger ShippingFee_Trigger
+After update of CustType on Customer
+For Each Row
+Begin
+    Update CustOrder
+    Set ShippingFee = 0 
+    Where (Select CustId from Customer C Where C.CustType = 'Gold')= CustId;
+End;
+/
+
+ 
+
+
+
+
+CREATE OR REPLACE FUNCTION get_GoldCustShippingFee
+RETURN Integer IS
+    tmp Integer := 0;
+BEGIN
+    SELECT ShippingFee into tmp
+    from CustOrder
+    Where ( Select CustId from Customer  C Where C.CustType = 'Gold')= CustId;
+    return tmp;
+END;
+/
