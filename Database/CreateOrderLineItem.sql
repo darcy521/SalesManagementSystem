dcbd932d85982c48277b8e7e1d56b6f2653bbbe6
darcy521 <honglinya521@gmail.com>
2022-10-28 18:08:29 -0700

    Update CreateOrderLineItem

--- Database/CreateOrderLineItem.sql
+++ Database/CreateOrderLineItem.sql
@@ -1,24 +1,41 @@
+CREATE SEQUENCE LineId_seq
+    INCREMENT BY 1
+    START WITH 00000001
+    MINVALUE 00000001
+    MAXVALUE 99999999
+    CYCLE;
+
 
 CREATE PROCEDURE createOrderLineItem(custOrderId in Integer, StoreItemsId in Integer,
 customerId in Integer, dataOrdered in Date, numberOrdered in Integer, shippedDate in Date)
 IS
     tmp Integer := 0;
+    CustTypeTmp VARCHAR(10);
+    ShippingFee Integer;
+    OdrId Integer;
 Begin
     Select NumberOfCopies into tmp
     from StoreItems si Where si.StoreItemsId = StoreItemsId;
+--    Do not forget to display error message on front-end!
+--If number of item ordered > stock -> display error and exit
     IF (numberOrdered > tmp) THEN
-    dbms_output.put_line('Number of item Ordered is larger than stock, bookstore only has '
-    || tmp || 'copies');
+        dbms_output.put_line('Number of item Ordered is larger than stock, bookstore only has '
+        || tmp || 'copies');
+        return;
+    END IF;
+
+    Select CustTypeTmp into CustTypeTmp
+    from customer si Where customerId = si.CustId;
+    IF (CustTypeTmp = 'Gold') Then
+        ShippingFee := 0;
+    ELSE
+        ShippingFee := 10;
+    END IF;
+
+    Select OrderId into OdrId
+    From CustOrder Where customerId = custorder.CustId;
+
+    INSERT INTO OrderLineItem(LineId, StoreItemsId, Quantity, OrderId) 
+    values (LineId_seq.NEXTVAL, StoreItemsId, numberOrdered,OdrId);
 End;
 /
-
-
---CREATE OR REPLACE FUNCTION get_NumberOfCopies (n in Integer)
---Return Integer IS
---    tmp Integer := 0;
---Begin
---    Select NumberOfCopies into tmp 
---    from StoreItems si Where si.StoreItemsId = n;
---    Return tmp;
---End;
---/
