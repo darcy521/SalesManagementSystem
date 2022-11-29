2f50e98d1a7fb24d0e619507d1a3c8e5d8a61385
Honglin Chen <honglinya521@gmail.com>
2022-11-28 21:08:31 -0800

    connect database to front-end using oci and php

--- Front-end/Data_Transmission.php
+++ Front-end/Data_Transmission.php
@@ -4,10 +4,30 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <script src="jquery-3.6.1.min.js"></script>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
       <title>MasterRobot Bookstore</title>
    </head>
    <body>
+        <!-- This form shows storeItems -->
+        <form name="createCustOrder-form" method="post" action="http://localhost/data_transmission.php?show=true">
+            <h3>Show StoreItems</h3>
+
+            <input type="submit" name="submit" value="show">
+            
+            <input type="button" name="submit" value="close">
+            <button type="submit" value="close">
+            <?php 
+                closeStoreItems();
+            ?>
+            </button>
+            <?php 
+                if (isset($_GET['show'])) {
+                    showStoreItems();
+                }
+            ?>
+        </form>
+
          <!-- This form create customer order -->
         <form name="createCustOrder-form" method="post" action="">
             <h3>Create New Order</h3>
@@ -107,5 +127,89 @@
             <input type="submit" name="submit" value="Submit">
         </form>
 
+        <?php
+        // Create connection to Oracle
+        $conn = oci_pconnect("c##123", "123", "//localhost/xe");
+        if (!$conn) {
+        $m = oci_error();
+        echo $m['message'], "\n";
+        exit;
+        }
+        else {
+        print "Connected to Oracle!";
+        }
+        // Close the Oracle connection
+        oci_close($conn);
+        ?>
+
    </body>
 </html>
+<?php 
+function showStoreItems(){
+    //connect to your database. Type in your username, password and the DB path
+    $conn=oci_connect("c##123", "123", "//localhost/xe");
+    if(!$conn) {
+            print "<br> connection failed:";       
+        exit;
+    }		
+    $query = oci_parse($conn, "SELECT * FROM StoreItems");
+    
+    // Execute the query
+    oci_execute($query);
+    
+    echo "<table id='storeItems-table' border='1'>";
+        echo "<tr>";
+            echo "<td>StoreItemsId</td>";
+            echo "<td>ItemId</td>";
+            echo "<td>Price</td>";
+            echo "<td>ItemType</td>";
+            echo "<td>NumberOfCopies</td>";
+        echo "</tr>";
+        
+    while (($row = oci_fetch_array($query, OCI_BOTH)) != false) {		
+        echo "<tr>";
+            echo "<td>$row[0]</td>";
+            echo "<td>$row[1]</td>";
+            echo "<td>$row[2]</td>";
+            echo "<td>$row[3]</td>";
+            echo "<td>$row[4]</td>";
+            echo "</tr>";
+    }
+    echo "</table>";
+    OCILogoff($conn);	
+}
+
+function closeStoreItems(){
+    
+}
+
+function createNewOrder(){
+    $conn=oci_connect("c##123", "123", "//localhost/xe");
+    if(!$conn) {
+            print "<br> connection failed:";       
+        exit;
+    }		
+
+    $sql = 'BEGIN createCustOrder(:CustId); END;';
+    $stmt = oci_parse($conn,$sql);
+    
+    // Bind the input parameter
+    oci_bind_by_name($stmt,':CustId',$CustId,32);
+    
+    // Bind the output parameter
+    oci_bind_by_name($stmt,':message',$message,32);
+    
+    // Assign a value to the input 
+    $name = 'Harry';
+
+    // Execute the query
+    oci_execute($stmt);
+    
+    // $message is now populated with the output value
+    print "$message\n";
+    
+    OCILogoff($conn);
+}
+
+
+?>
